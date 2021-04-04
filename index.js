b70b434d2e13d3e5c2c4f228f9d105a15b2c6ce8
Candice Scott-Mason <cscottmason23@gmail.com>
2021-04-04 19:41:11 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const people = ["Lisa", "Kaitlyn", "Jan"]
+
+function writeCards(people, events) {
+   let thankYouCards = []
+    for(let i = 0; i < people.length; i ++){
+        thankYouCards.push(`Thank you, ${people[i]}, for the wonderful surprise gift!`)
+    }
+    return thankYouCards;
+}
+
+function countDown(num) {
+    while (num > 0) {
+        console.log (num);
+        num -= 1;
+    }
+    console.log(num);
+}
